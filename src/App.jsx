c10fce8556f74c800f28aaa8ba30a4a0173a5efc
MostafaKhfagy <mostafakhfagy4@gmail.com>
2025-05-06 21:57:27 +0300

    Refactor routing in App component to integrate admin layout and some pages
    - including admin `Dashboard` home page
    - `Users` admin page
    - `ShowAdminPage` admin page

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Canvas } from '@react-three/fiber';
 import ShoesShop from './Pages/ShoesShop';
 
 import { noEvents } from '@react-three/xr';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { PCFSoftShadowMap } from 'three';
 import { Suspense } from "react";
+import Layout from "./Admin/Layout/Layout.jsx";
+import Users from './Admin/Users/Users';
+import ShowAdminPage from './Admin/ShowAdminPage/ShowAdminPage';
 import Login from "./Components/Form/Login/Login";
 import SignUp from "./Components/Form/SignUp/Signup";
 import ForgetPassword from "./Components/Form/ForgotPassword/Forgotpassword";
@@ -14,7 +17,6 @@ import Home from "./Components/HomePage/Home/Home";
 import Cart from "./Pages/Cart";
 import Profile from "./Components/HomePage/Profile/Profile";
 import SportsShop from "./Pages/SportStore";
-import AdminDashboard from "./Pages/AdminDashboard";
 import ClientShop from "./Pages/ClientShop";
 import Unauthorized from "./Pages/Unauthorized";
 import ProtectedRoute from "./Components/ProtectedRoute";
@@ -22,8 +24,11 @@ import Activate from "./Components/Form/ActivateAccount/activateAccount";
 import ResetPassword from "./Components/Form/ForgotPassword/ResetPassword";
 import Loader from './Utils/Loader/Loader';
 import { Holding } from './Utils/Holding';
+import Dashboard from './Admin/Dashboard/Dashboard.jsx';
 
 export const App = () => {
+  const { pathname } = useLocation();
+
   return (
     <AuthProvider>
       <Routes>
@@ -42,15 +47,16 @@ export const App = () => {
               <Home />
             </ProtectedRoute>
           }
-          />
-        <Route
-          path="/admin-dashboard"
-          element={
-            <ProtectedRoute allowedRoles={["ROLE_ADMIN"]}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-          />
+        />
+        {/* <ProtectedRoute allowedRoles={["ROLE_ADMIN"]}> */}
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={
+            <Dashboard />
+          } />
+          <Route path="users" element={<Users />} />
+          <Route path="showAdmin" element={<ShowAdminPage />} />
+        </Route>
+        {/* </ProtectedRoute> */}
         <Route
           path="/client-shop"
           element={
@@ -58,29 +64,34 @@ export const App = () => {
               <ClientShop />
             </ProtectedRoute>
           }
-          />
+        />
         <Route path="/unauthorized" element={<Unauthorized />} />
       </Routes>
-      
-      <Canvas
-        style={{ width: '100vw', height: '100vh', flexGrow: 1 }}
-        gl={{ antialias: true }}
-        shadows="soft"
-        camera={{ position: [-0.5, 0.5, 0.5] }}
-        events={noEvents}
-        onCreated={({ gl }) => {
-          gl.shadowMap.enabled = true;
-          gl.shadowMap.type = PCFSoftShadowMap;
-        }}
-        >
-        <Suspense fallback={<Loader/>}>
-          <Routes>
-              <Route path='/Room' element={<Holding/>}></Route>
-              <Route path="shoes-shop" element={<ShoesShop />}></Route>
-              <Route path="sports-shop" element={<SportsShop />}></Route>
-          </Routes>
-        </Suspense> 
-      </Canvas>
+      {/* hide th canva if pathname is /admin or any sub */}
+      {
+        pathname === "/admin" || pathname.startsWith("/admin/") ? null : (
+          <Canvas
+            style={{ width: '100vw', height: '100vh', flexGrow: 1 }}
+            gl={{ antialias: true }}
+            shadows="soft"
+            camera={{ position: [-0.5, 0.5, 0.5] }}
+            events={noEvents}
+            onCreated={({ gl }) => {
+              gl.shadowMap.enabled = true;
+              gl.shadowMap.type = PCFSoftShadowMap;
+            }}
+          >
+            <Suspense fallback={<Loader />}>
+              <Routes>
+                <Route path='/Room' element={<Holding />}></Route>
+                <Route path="shoes-shop" element={<ShoesShop />}></Route>
+                <Route path="sports-shop" element={<SportsShop />}></Route>
+              </Routes>
+            </Suspense>
+          </Canvas>
+        )
+      }
+
     </AuthProvider>
   );
 };
